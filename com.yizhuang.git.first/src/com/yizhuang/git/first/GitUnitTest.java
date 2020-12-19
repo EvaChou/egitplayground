47dc99282077181ff48671af5231ef528ef7b93e
yizhuang <yizhuang@10.6.200.64>
2020-12-19 15:19:34 -0800

    Add unit tests

---
+++ com.yizhuang.git.first/src/com/yizhuang/git/first/GitUnitTest.java
@@ -0,0 +1,14 @@
+package com.yizhuang.git.first;
+
+import static org.junit.Assert.fail;
+
+import org.junit.jupiter.api.Test;
+
+class GitUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- com.yizhuang.git.first/src/module-info.java
+++ com.yizhuang.git.first/src/module-info.java
@@ -1,2 +1,4 @@
 module com.yizhuang.git.first {
+	requires org.junit.jupiter.api;
+	requires junit;
 }
