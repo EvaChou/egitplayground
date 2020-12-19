b87aac52d76bbf00f46e2d1256959f47840b37a6
yizhuang <yizhuang@10.6.200.64>
2020-12-19 14:47:34 -0800

    Add one output line

--- com.yizhuang.git.first/src/com/yizhuang/git/first/GitTest.java
+++ com.yizhuang.git.first/src/com/yizhuang/git/first/GitTest.java
@@ -3,5 +3,6 @@ package com.yizhuang.git.first;
 public class GitTest {
 	public static void main(String[] args) {
 	    System.out.println("Git is cool and fun and awesome");
+	    System.out.println();
 	  }
 }
