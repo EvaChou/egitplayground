69aaee88139554c66629988f2e4ccefd5409df8d
yizhuang <yizhuang@10.6.200.64>
2020-12-19 14:40:05 -0800

    test

--- com.yizhuang.git.first/src/com/yizhuang/git/first/GitTest.java
+++ com.yizhuang.git.first/src/com/yizhuang/git/first/GitTest.java
@@ -2,6 +2,6 @@ package com.yizhuang.git.first;
 
 public class GitTest {
 	public static void main(String[] args) {
-	    System.out.println("Git is cool and fun");
+	    System.out.println("Git is cool and fun and awesome");
 	  }
 }
